41b44408ab0e43217f397680977b1503ad1602c0
SebastianPerilla <sebastianperillaespinosa@gmail.com>
2025-04-23 00:01:42 +0200

    First Commit for Model Deployment

--- Home.py
+++ Home.py
@@ -1,20 +1,5 @@
-import streamlit as st
-from utils.feature_extractor import BookFeatureExtractor, load_book
+import streamlit as st  
 
 st.set_page_config(page_title='Book Analysis', layout='wide')
 st.title('📚 Book Feature Extraction')
 
-uploaded = st.file_uploader('Upload a text or PDF file', type=['txt','pdf'])
-if uploaded:
-    # save to temp
-    temp = f"/tmp/{uploaded.name}"
-    with open(temp, 'wb') as f:
-        f.write(uploaded.getbuffer())
-    text = load_book(temp)
-
-    with st.spinner('Analyzing...'):
-        extractor = BookFeatureExtractor()
-        profile = extractor.extract_book_profile(text)
-
-    st.subheader('Overview')
-    st.json(profile)
